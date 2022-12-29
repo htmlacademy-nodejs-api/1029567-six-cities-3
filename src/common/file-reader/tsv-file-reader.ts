086596e071e22d0bf7dd8931ce6aed36517ac9e3
Anna_Podkasik <ostretsova.ann@gmail.com>
2022-12-29 20:47:20 +0300

    add import tsv

--- src/common/file-reader/tsv-file-reader.ts
+++ src/common/file-reader/tsv-file-reader.ts
@@ -2,8 +2,8 @@ import { readFileSync } from 'fs';
 import { OfferType } from '../../types/offer-type.enum.js';
 import { Offer } from '../../types/offer.type.js';
 import { FileReaderInterface } from './file-reader.interface.js';
-import { FacilitiesType } from '../../types/offer-facilities.enum';
-import { UserType } from '../../types/user-type.enum';
+import { FacilitiesType } from '../../types/offer-facilities.enum.js';
+import { UserType } from '../../types/user-type.enum.js';
 
 export default class TSVFileReader implements FileReaderInterface {
   private rawData = '';
@@ -29,19 +29,19 @@ export default class TSVFileReader implements FileReaderInterface {
         postDate: new Date(createdDate),
         city,
         previewImage,
-        offerImages,
-        isPremium,
-        isFavorites,
-        rating,
+        offerImages: offerImages.split(';'),
+        isPremium: Boolean(isPremium),
+        isFavorites: Boolean(isFavorites),
+        rating: Number.parseInt(rating, 10),
         type: OfferType[type as 'apartment' | 'house' | 'room' | 'hotel'],
-        roomCount,
-        guestsCount,
-        price,
+        roomCount: Number.parseInt(roomCount, 10),
+        guestsCount: Number.parseInt(guestsCount, 10),
+        price: Number.parseInt(price, 10),
         facilities: facilities.split(';')
-          .map(x => FacilitiesType[x as 'Breakfast' | 'AirConditioning' | 'LaptopFriendlyWorkspace' | 'BabySeat' | 'Washer' | 'Towels' | 'Fridge'])
-        user: { name, email, avatarPath, password, userType: UserType[type as 'normal' | 'pro'] },
-        commentCount,
-        coordinates,
+          .map((x) => FacilitiesType[x as 'Breakfast' | 'AirConditioning' | 'LaptopFriendlyWorkspace' | 'BabySeat' | 'Washer' | 'Towels' | 'Fridge']),
+        user: { name, email, avatarPath, password, userType: UserType[userType as 'normal' | 'pro'] },
+        commentCount: Number.parseInt(commentCount, 10),
+        coordinates: coordinates.split(';'),
       }));
   }
 }
