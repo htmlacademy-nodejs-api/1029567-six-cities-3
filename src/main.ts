8d0ddc9dc50d698b0fe2cec7b94340e4a7953035
Podkasik Anna <ostretsova.ann@gmail.com>
2023-01-30 21:18:37 +0300

    add services to database

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
 import { applicationContainer } from './app/application.container.js';
+import { commentContainer } from './modules/comment/comment.container.js';
 import Application from './app/application.js';
 import { Component } from './types/component.types.js';
 import { userContainer } from './modules/user/user.container.js';
 
 const mainContainer = Container.merge(
   applicationContainer,
-  userContainer
+  userContainer,
+  commentContainer
 );
 
 async function bootstrap() {
